ea4e399f2a505a0929ffdff8a05b8df073ef4dea
JoshHeida <joshheida@gmail.com>
2022-06-05 18:01:34 -0400

    added bootstrap/fontawesome

--- server.ts
+++ server.ts
@@ -10,30 +10,22 @@ import http from 'http';
 import { HttpError } from 'http-errors';
 
 
-/**
- * Get port from environment and store in Express.
- */
+
 
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
+
 
 const server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
+
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
+
 
 function normalizePort(val:string) {
   let port = parseInt(val, 10);
