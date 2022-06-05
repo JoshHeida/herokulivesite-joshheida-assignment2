51d75168e93e25475f281da742ed89788c845060
JoshHeida <joshheida@gmail.com>
2022-06-05 19:21:01 -0400

    express template intial

--- Server/Routes/index.ts
+++ Server/Routes/index.ts
@@ -3,7 +3,26 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' , page:'home'});
 });
+router.get('/home', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'Home' ,page:'home'});
+});
+//about page
+router.get('/about',function(req: express.Request, res: express.Response, next: express.NextFunction){
+  res.render('index',{title:'About', page:'about'})
+})
+//services page
+router.get('/services',function(req: express.Request, res: express.Response, next: express.NextFunction){
+  res.render('index',{title:'Services', page:'services'})
+})
+//projects page
+router.get('/projects',function(req: express.Request, res: express.Response, next: express.NextFunction){
+  res.render('index',{title:'Projects', page:'projects'})
+})
+//contact page
+router.get('/contact',function(req: express.Request, res: express.Response, next: express.NextFunction){
+  res.render('index',{title:'Contact', page:'contact'})
+})
 
 export default router;
