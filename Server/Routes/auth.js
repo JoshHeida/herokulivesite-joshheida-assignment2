d9cefd1d5a9b1c8bdb9ff2a54fff0dcfcbd4d3ee
JoshHeida <joshheida@gmail.com>
2022-06-20 10:30:20 -0400

    removed register, login css

--- Server/Routes/auth.js
+++ Server/Routes/auth.js
@@ -7,9 +7,7 @@ const express_1 = __importDefault(require("express"));
 const auth_1 = require("../Controllers/auth");
 const router = express_1.default.Router();
 router.get('/login', auth_1.DisplayLogin);
-router.get('/register', auth_1.DisplayRegister);
 router.post('/login', auth_1.ProcessLogin);
-router.post('/register', auth_1.ProcessRegister);
 router.get('/logout', auth_1.ProcessLogout);
 exports.default = router;
 //# sourceMappingURL=auth.js.map
--- Server/Routes/auth.ts
+++ Server/Routes/auth.ts
@@ -5,13 +5,11 @@ const router = express.Router();
 
 /* GET login page. */
 router.get('/login', DisplayLogin);
-//register page
-router.get('/register', DisplayRegister);
+
 
 //process login
 router.post('/login',ProcessLogin);
-//process register
-router.post('/register',ProcessRegister);
+
 //process logout
 router.get('/logout',ProcessLogout);
 
