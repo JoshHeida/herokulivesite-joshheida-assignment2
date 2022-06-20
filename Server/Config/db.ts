0a0a9fb582768784609ba0faa282f89a4dbb16b9
JoshHeida <joshheida@gmail.com>
2022-06-20 01:05:24 -0400

    db bug

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://127.0.0.1/contacts";
-export const RemoteURI = process.env.RemoteURI;
+export const RemoteURI = mongodb+srv://Admin:admin@cluster0.95nyj.mongodb.net/?retryWrites=true&w=majority;
 export const HostName = "MongoDB Atlas";
 export const Secret = "someSecret";
